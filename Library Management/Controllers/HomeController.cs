9c6c8f2ac1df3fda89284e71fa47c5cf626ca837
Eduard-Stefan <cojocaru.eduard.p6b@student.ucv.ro>
2024-04-13 12:53:16 +0300

    CRUD Subsidiaries

--- Library Management/Controllers/HomeController.cs
+++ Library Management/Controllers/HomeController.cs
@@ -1,16 +1,19 @@
 using Library_Management.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 
 namespace Library_Management.Controllers
 {
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+		private readonly ApplicationDbContext _context;
 
-		public HomeController(ILogger<HomeController> logger)
+		public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
 		{
 			_logger = logger;
+			_context = context;
 		}
 
 		public IActionResult Index()
@@ -18,9 +21,19 @@ namespace Library_Management.Controllers
 			return View();
 		}
 
-		public IActionResult Privacy()
+		public IActionResult Contact()
 		{
 			return View();
+		}
+
+		public IActionResult Books()
+		{
+			return View();
+		}
+
+		public async Task<IActionResult> Subsidiaries()
+		{
+			return View(await _context.Subsidiaries.ToListAsync());
 		}
 
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
