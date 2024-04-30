e2c0521f6b99e1cf48196ee3a6b085c6b13487e9
Eduard-Stefan <cojocaru.eduard.p6b@student.ucv.ro>
2024-04-30 20:21:58 +0300

    Small bug fix for unwelcome users

--- Library Management/Repositories/UserRepository.cs
+++ Library Management/Repositories/UserRepository.cs
@@ -14,14 +14,14 @@ namespace Library_Management.Repositories
 		public IQueryable<User> UnwelcomeUsers()
 		{
 			var currentDate = DateTime.Now;
-			ApplicationDbContext.Users.AsNoTracking()
+			ApplicationDbContext.Users
 				.Where(user => user.BorrowedBooks.Any(borrowedBook => borrowedBook.ReturnDate < currentDate))
 				.ToList()
 				.ForEach(user => user.IsWelcome = false);
 
 			ApplicationDbContext.SaveChanges();
 
-			return ApplicationDbContext.Users.AsNoTracking()
+			return ApplicationDbContext.Users
 				.Where(user => user.IsWelcome == false);
 		}
 	}
