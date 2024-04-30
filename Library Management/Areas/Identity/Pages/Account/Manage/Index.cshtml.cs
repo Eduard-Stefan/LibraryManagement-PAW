0e893c492745c0e9ce278c9580d5ca6cac369291
Eduard-Stefan <cojocaru.eduard.p6b@student.ucv.ro>
2024-04-30 14:02:50 +0300

    Users can now remove their profile picture

--- Library Management/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ Library Management/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -1,76 +1,92 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
-using System.Text.Encodings.Web;
+﻿using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Threading.Tasks;
 using Library_Management.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Library_Management.Areas.Identity.Pages.Account.Manage
 {
-    public class IndexModel : PageModel
-    {
-        private readonly UserManager<User> _userManager;
-        private readonly SignInManager<User> _signInManager;
+	public class IndexModel : PageModel
+	{
+		private readonly UserManager<User> _userManager;
+		private readonly IWebHostEnvironment _environment;
 
-        public User? user;
+		public User? user;
 
-        public IndexModel(
-            UserManager<User> userManager,
-            SignInManager<User> signInManager)
-        {
-            _userManager = userManager;
-            _signInManager = signInManager;
-        }
+		public IndexModel(
+			UserManager<User> userManager,
+			IWebHostEnvironment environment)
+		{
+			_userManager = userManager;
+			_environment = environment;
+		}
 
-        [TempData]
-        public string StatusMessage { get; set; }
+		[BindProperty] public IFormFile ImageFile { get; set; }
 
-        public async Task<IActionResult> OnGetAsync()
-        {
-            user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
+		[TempData] public string StatusMessage { get; set; }
 
-            return Page();
-        }
+		private const string DefaultImageFileName = "default.png";
 
-        public async Task<IActionResult> OnPostUploadImageAsync()
-        {
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
+		public async Task<IActionResult> OnGetAsync()
+		{
+			var userTask = _userManager.GetUserAsync(User);
+			userTask.Wait();
+			user = userTask.Result;
 
-            var imageFile = Request.Form.Files["ImageFile"];
-            if (imageFile != null && imageFile.Length > 0)
-            {
-                // Save the image file
-                var fileName = $"{Guid.NewGuid()}{System.IO.Path.GetExtension(imageFile.FileName)}";
-                var imagePath = System.IO.Path.Combine("wwwroot", "profile-images", fileName);
-                using (var fileStream = new System.IO.FileStream(imagePath, System.IO.FileMode.Create))
-                {
-                    await imageFile.CopyToAsync(fileStream);
-                }
+			return Page();
+		}
 
-                // Update the user's profile picture
-                user.ImageFileName = fileName;
-                var result = await _userManager.UpdateAsync(user);
-                if (result.Succeeded)
-                {
-                    StatusMessage = "Profile picture updated successfully.";
-                }
-                else
-                {
-                    StatusMessage = "Error updating profile picture.";
-                }
-            }
+		public async Task<IActionResult> OnPostUploadImageAsync()
+		{
+			var user = await _userManager.GetUserAsync(User);
+			if (user == null)
+			{
+				return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+			}
 
-            return RedirectToPage();
-        }
-    }
+			if (ImageFile != null)
+			{
+				string uploadsFolder = Path.Combine(_environment.WebRootPath, "profile-images");
+				string uniqueFileName = $"{user.Id}_{ImageFile.FileName}";
+				string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+				using (var stream = new FileStream(filePath, FileMode.Create))
+				{
+					await ImageFile.CopyToAsync(stream);
+				}
+
+				user.ImageFileName = uniqueFileName;
+				await _userManager.UpdateAsync(user);
+			}
+
+			return RedirectToPage();
+		}
+
+		public async Task<IActionResult> OnPostRemoveImageAsync()
+		{
+			var user = await _userManager.GetUserAsync(User);
+			if (user == null)
+			{
+				return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+			}
+
+			// Remove the current profile picture
+			string uploadsFolder = Path.Combine(_environment.WebRootPath, "profile-images");
+			string filePath = Path.Combine(uploadsFolder, user.ImageFileName);
+			if (System.IO.File.Exists(filePath))
+			{
+				System.IO.File.Delete(filePath);
+			}
+
+			// Revert to the default image
+			user.ImageFileName = DefaultImageFileName;
+			await _userManager.UpdateAsync(user);
+
+			return RedirectToPage();
+		}
+	}
 }
